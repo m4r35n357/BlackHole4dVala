47db93b38469ff712338ae2929d6abcfcc16b7f5
Ian Smith <m4r35n357@gmail.com>
2014-10-06 11:13:13 +0100

    initial commit

---
+++ bh3d.py
@@ -0,0 +1,144 @@
+#!/usr/bin/env pypy
+
+from sys import stdin, stdout, stderr
+from math import fabs, log10, sqrt, sin, cos
+from json import loads
+from array import array
+
+class BL(object):
+
+    def __init__(self, mass, spin, energy, momentum, carter, r0, theta0, phi0, simtime, timestep):
+    	self.m = mass
+    	self.a = spin
+        self.mu = 1.0
+    	self.E = energy
+    	self.Lz = momentum
+    	self.Q = carter
+    	self.r = r0
+    	self.theta = theta0
+    	self.phi = phi0
+    	self.time = simtime
+    	self.step = timestep
+        self.t = 0.0
+        self.n = simtime / fabs(timestep)  # We can run backwards too!
+        self.eMax = 30.0
+        self.L_aE = self.Lz - self.a * self.E
+
+# intermediates
+    def sigma (self, r, theta):
+    	return r**2 + self.a**2 * cos(theta)**2
+
+    def delta (self, r):
+    	return r**2 - 2.0 * r * self.m + self.a**2
+
+    def P1 (self, r):
+    	return (r**2 + self.a**2) * self.E - self.a * self.Lz
+
+    def P2(self, r):
+    	return self.mu**2 * r**2 + self.L_aE**2 + self.Q
+
+    def R (self, r):
+    	return self.P1(r)**2 - self.delta(r) * self.P2(r)
+
+    def T1 (self, theta):
+    	return self.a**2 * (self.mu**2 - self.E**2) + self.Lz**2 / sin(theta)**2
+
+    def THETA (self, theta):
+    	return self.Q - cos(theta)**2 * self.T1(theta)
+
+# hamiltonian
+    def h (self, r, theta):
+        return 0.5 * (self.delta(r) * self.pR**2 + self.pTh**2 - self.R(r) / self.delta(r) - self.THETA(theta) - 1.0)
+
+# parameters
+    def tDeriv (self, t, r, theta, phi, pR, pTh):
+        return (r**2 + self.a**2) * self.P1(r) / self.delta(r) + self.a * self.L_aE + cos(theta)**2 * self.a**2 * self.E
+
+    def rDeriv (self, t, r, theta, phi, pR, pTh):
+        return self.delta(r) * pR
+
+    def thetaDeriv (self, t, r, theta, phi, pR, pTh):
+        return pTh
+
+    def phiDeriv (self, t, r, theta, phi, pR, pTh):
+        return self.a * self.P1(r) / self.delta(r) + self.L_aE + self.Lz * cos(theta)**2 / sin(theta)**2
+
+# derivatives
+    def rDotDeriv (self, t, r, theta, phi, pR, pTh):
+        return (r - 1.0) * self.P1(r)**2 / self.delta(r)**2 - 2.0 * r * self.E * self.P1(r) / self.delta(r) + (1.0 - r ) * pR**2 + r
+
+    def thDotDeriv (self, t, r, theta, phi, pR, pTh):
+        return cos(theta) * sin(theta) * self.T1(theta) + self.Lz**2 * cos(theta)**3 / sin(theta)**3
+
+# RK4
+    def iterate (self, t, r, theta, phi, pR, pTh):
+        a = array('d', [self.tDeriv (t, r, theta, phi, pR, pTh),
+                 self.rDeriv (t, r, theta, phi, pR, pTh),
+                 self.thetaDeriv (t, r, theta, phi, pR, pTh),
+                 self.phiDeriv (t, r, theta, phi, pR, pTh),
+                 self.rDotDeriv (t, r, theta, phi, pR, pTh),
+                 self.thDotDeriv (t, r, theta, phi, pR, pTh)])
+        b = array('d', [self.tDeriv (t + 0.5 * self.step * a[0], r + 0.5 * a[1], theta + 0.5 * self.step * a[2], phi + 0.5 * self.step * a[3], pR + 0.5 * self.step * a[4], pTh + 0.5 * self.step * a[5]),
+                 self.rDeriv (t + 0.5 * self.step * a[0], r + 0.5 * self.step * a[1], theta + 0.5 * self.step * a[2], phi + 0.5 * self.step * a[3], pR + 0.5 * self.step * a[4], pTh + 0.5 * self.step * a[5]),
+                 self.thetaDeriv (t + 0.5 * self.step * a[0], r + 0.5 * self.step * a[1], theta + 0.5 * self.step * a[2], phi + 0.5 * self.step * a[3], pR + 0.5 * self.step * a[4], pTh + 0.5 * self.step * a[5]),
+                 self.phiDeriv (t + 0.5 * self.step * a[0], r + 0.5 * self.step * a[1], theta + 0.5 * self.step * a[2], phi + 0.5 * self.step * a[3], pR + 0.5 * self.step * a[4], pTh + 0.5 * self.step * a[5]),
+                 self.rDotDeriv (t + 0.5 * self.step * a[0], r + 0.5 * self.step * a[1], theta + 0.5 * self.step * a[2], phi + 0.5 * self.step * a[3], pR + 0.5 * self.step * a[4], pTh + 0.5 * self.step * a[5]),
+                 self.thDotDeriv (t + 0.5 * self.step * a[0], r + 0.5 * self.step * a[1], theta + 0.5 * self.step * a[2], phi + 0.5 * self.step * a[3], pR + 0.5 * self.step * a[4], pTh + 0.5 * self.step * a[5])])
+        c = array('d', [self.tDeriv (t + 0.5 * self.step * b[0], r + 0.5 * self.step * b[1], theta + 0.5 * self.step * b[2], phi + 0.5 * self.step * b[3], pR + 0.5 * self.step * b[4], pTh + 0.5 * self.step * b[5]),
+                 self.rDeriv (t + 0.5 * self.step * b[0], r + 0.5 * self.step * b[1], theta + 0.5 * self.step * b[2], phi + 0.5 * self.step * b[3], pR + 0.5 * self.step * b[4], pTh + 0.5 * self.step * b[5]),
+                 self.thetaDeriv (t + 0.5 * self.step * b[0], r + 0.5 * self.step * b[1], theta + 0.5 * self.step * b[2], phi + 0.5 * self.step * b[3], pR + 0.5 * self.step * b[4], pTh + 0.5 * self.step * b[5]),
+                 self.phiDeriv (t + 0.5 * self.step * b[0], r + 0.5 * self.step * b[1], theta + 0.5 * self.step * b[2], phi + 0.5 * self.step * b[3], pR + 0.5 * self.step * b[4], pTh + 0.5 * self.step * b[5]),
+                 self.rDotDeriv (t + 0.5 * self.step * b[0], r + 0.5 * self.step * b[1], theta + 0.5 * self.step * b[2], phi + 0.5 * self.step * b[3], pR + 0.5 * self.step * b[4], pTh + 0.5 * self.step * b[5]),
+                 self.thDotDeriv (t + 0.5 * self.step * b[0], r + 0.5 * self.step * b[1], theta + 0.5 * self.step * b[2], phi + 0.5 * self.step * b[3], pR + 0.5 * self.step * b[4], pTh + 0.5 * self.step * b[5])])
+        d = array('d', [self.tDeriv (t + self.step * c[0], r + self.step * c[1], theta + self.step * c[2], phi + self.step * c[3], pR + self.step * c[4], pTh + self.step * c[5]),
+                 self.rDeriv (t + self.step * c[0], r + self.step * c[1], theta + self.step * c[2], phi + self.step * c[3], pR + self.step * c[4], pTh + self.step * c[5]),
+                 self.thetaDeriv (t + self.step * c[0], r + self.step * c[1], theta + self.step * c[2], phi + self.step * c[3], pR + self.step * c[4], pTh + self.step * c[5]),
+                 self.phiDeriv (t + self.step * c[0], r + self.step * c[1], theta + self.step * c[2], phi + self.step * c[3], pR + self.step * c[4], pTh + self.step * c[5]),
+                 self.rDotDeriv (t + self.step * c[0], r + self.step * c[1], theta + self.step * c[2], phi + self.step * c[3], pR + self.step * c[4], pTh + self.step * c[5]),
+                 self.thDotDeriv (t + self.step * c[0], r + self.step * c[1], theta + self.step * c[2], phi + self.step * c[3], pR + self.step * c[4], pTh + self.step * c[5])])
+        self.t += self.step * (a[0] + 2.0 * b[0] + 2.0 * c[0] + d[0]) / 6.0
+        self.r += self.step * (a[1] + 2.0 * b[1] + 2.0 * c[1] + d[1]) / 6.0
+        self.theta += self.step * (a[2] + 2.0 * b[2] + 2.0 * c[2] + d[2]) / 6.0
+        self.phi += self.step * (a[3] + 2.0 * b[3] + 2.0 * c[3] + d[3]) / 6.0
+        self.pR += self.step * (a[4] + 2.0 * b[4] + 2.0 * c[4] + d[4]) / 6.0
+        self.pTh += self.step * (a[5] + 2.0 * b[5] + 2.0 * c[5] + d[5]) / 6.0
+
+    def print_out (self, time, hNow, h0, hMin, hMax, dbValue):
+	print >> stdout, '{"tau":%.9e, "t":%.9e, "r":%.9e, "th":%.9e, "ph":%.9e}' % (-self.step, self.t, self.r, self.theta, self.phi)  # Log data
+	print >> stderr, '{"t":%.2f, "H":%.9e, "H0":%.9e, "H-":%.9e, "H+":%.9e, "ER":%.1f}' % (time, hNow, h0, hMin, hMax, dbValue)  # Log progress
+
+
+# parse input
+def icJson ():
+	ic = loads(stdin.read())
+        return BL(ic['M'], ic['a'], ic['E'], ic['Lz'], ic['C'], ic['r'], ic['theta'], ic['phi'], ic['time'], ic['step'])
+
+def main ():  # Need to be inside a function to return . . .
+    bh = icJson()
+    bh.pR = sqrt(bh.R(bh.r)) / bh.delta(bh.r) if (bh.R(bh.r) >= 0.0) else -sqrt(-bh.R(bh.r)) / delta(bh.r)
+    bh.pTh = sqrt(bh.THETA(bh.theta)) if (bh.THETA(bh.theta) >= 0.0) else -sqrt(-bh.THETA(bh.theta))
+#    bh.iterate(bh.t, bh.r, bh.theta, bh.phi, bh.pR, bh.pTh)
+    h0 = hMax = hMin = bh.h(bh.r,  bh.theta)  # Set up error reporting
+    bh.print_out(0.0, h0, h0, h0, h0, -180.0)
+    n = 1
+    while n <= bh.n:
+        bh.iterate(bh.t, bh.r, bh.theta, bh.phi, bh.pR, bh.pTh)
+#	s.solve()  # Perform one full integration step
+	hNow = bh.h(bh.r, bh.theta)		
+	tmp = fabs(hNow - h0)  # Protect logarithm against negative arguments
+	dH = tmp if tmp > 0.0 else 1.0e-18  # Protect logarithm against small arguments
+	if hNow < hMin:  # Low tide
+		hMin = hNow
+	elif hNow > hMax:  # High tide
+		hMax = hNow
+	dbValue = 10.0 * log10(dH)
+	bh.print_out(n * bh.step, hNow, h0, hMin, hMax, dbValue)
+	if dbValue > bh.eMax:
+		return
+	n += 1
+
+if __name__ == "__main__":
+    main()
+else:
+    print >> stderr, __name__ + " module loaded"
+
