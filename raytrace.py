fcc01c0805940a99b755aa00e4531cac422d4bc5
Ian Smith <m4r35n357@gmail.com>
2014-11-11 15:44:16 +0000

    raytracing pipeline

--- raytrace.py
+++ raytrace.py
@@ -1,21 +1,20 @@
 #!/usr/bin/env python
 
-from sys import argv, stdout
+from sys import argv, stdin, stdout
 from math import fabs, log10, sqrt, sin, cos, pi
 from json import loads
 
 def main():
-	if len(argv) < 2:
-		raise Exception('>>> ERROR! Please supply a data file name <<<')
-	dataFile = open(argv[1], 'r')
-	interval = int(argv[2])
-	line = dataFile.readline()
+	if len(argv) < 1:
+		raise Exception('>>> ERROR! Please supply a plotting poing interval <<<')
+	interval = int(argv[1])
+	line = stdin.readline()
 	n = 0
 	while line:
 		p = loads(line)
 		if (n % interval == 0):
 			print >> stdout, str(p['tau']) + ' 2 ' + str(p['r']) + ' ' + str(cos(p['th'])) + ' ' + str(p['t']) + ' ' + str(p['ph']) + ' ' + str(sqrt(p['R'])) + ' ' + str(- sin(p['th']) * sqrt(p['THETA'])) + ' ' + str(p['tDot']) + ' ' + str(p['phDot']) + ' ' + str(-1) + ' ' + str(0) + ' ' + str(0) + ' ' + str(0) + ' ' + str(0) + ' ' + str(0) + ' ' + str(0) + ' ' + str(1) + ' ' + str(-1) + ' ' + str(1) + ' ' + str(0) + ' ' + str(0)
-		line = dataFile.readline()
+		line = stdin.readline()
 		n += 1
 
 if __name__ == "__main__":
