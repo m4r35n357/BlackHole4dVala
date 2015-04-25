8905b193cf246c0040eadb3669f89ca9bab4faa9
Ian Smith <m4r35n357@gmail.com>
2015-04-25 16:16:44 +0100

    reformatting source to make it easier to see kerr-image parameters

--- raytrace.py
+++ raytrace.py
@@ -13,7 +13,12 @@ def main():
 	while line:
 		p = loads(line)
 		if (n % interval == 0):
-			print >> stdout, str(p['tau']) + ' 2 ' + str(p['r']) + ' ' + str(cos(p['th'])) + ' ' + str(p['t']) + ' ' + str(p['ph']) + ' ' + str(p['rDot']) + ' ' + str(- sin(p['th']) * p['thDot']) + ' ' + str(p['tDot']) + ' ' + str(p['phDot']) + ' ' + str(-1) + ' ' + str(0) + ' ' + str(0) + ' ' + str(0) + ' ' + str(0) + ' ' + str(0) + ' ' + str(0) + ' ' + str(1) + ' ' + str(0) + ' ' + str(1) + ' ' + str(0) + ' ' + str(0)
+			print >> stdout, str(p['tau']) + ' 2 ' \
+				+ str(p['r']) + ' ' + str(cos(p['th'])) + ' ' + str(p['t']) + ' ' + str(p['ph']) + ' ' \
+				+ str(p['rDot']) + ' ' + str(- sin(p['th']) * p['thDot']) + ' ' + str(p['tDot']) + ' ' + str(p['phDot']) + ' ' \
+				+ str(-1) + ' ' + str(0) + ' ' + str(0) + ' ' + str(0) + ' ' \
+				+ str(0) + ' ' + str(0) + ' ' + str(0) + ' ' + str(1) + ' ' \
+				+ str(0) + ' ' + str(1) + ' ' + str(0) + ' ' + str(0)
 		line = stdin.readline()
 		n += 1
 
