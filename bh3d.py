4e127cf946f3fea378eddd272a6e7a59bb1d32c4
Ian Smith <m4r35n357@gmail.com>
2015-05-16 11:37:40 +0100

    simplify theta input handling

--- bh3d.py
+++ bh3d.py
@@ -148,7 +148,8 @@ def main ():  # Need to be inside a function to return . . .
         bl.errors()
         ra = sqrt(bl.r**2 + bl.a2)
 	sigma = (bl.r**2 + bl.a2 * cos(bl.th)**2)
-	print >> stdout, '{"mino":%.9e, "tau":%.9e, "E":%.1f, "ER":%.1f, "ETh":%.1f, "EC":%.1f, "t":%.9e, "r":%.9e, "th":%.9e, "ph":%.9e, "tDot":%.9e, "rDot":%.9e, "thDot":%.9e, "phDot":%.9e, "x":%.9e, "y":%.9e, "z":%.9e}' % (bl.mino, bl.tau, bl.e, bl.eR, bl.eTh, 10.0 * log10(bl.eCum if bl.eCum >= bl.nf else bl.nf), bl.t, bl.r, bl.th, bl.ph, bl.tDot / sigma, bl.vR / sigma, bl.vTh / sigma, bl.phDot / sigma, ra * bl.sth * cos(bl.ph), ra * bl.sth * sin(bl.ph), bl.r * bl.cth)  # Log data
+	print >> stdout, '{"mino":%.9e, "tau":%.9e, "E":%.1f, "ER":%.1f, "ETh":%.1f, "EC":%.1f, "t":%.9e, "r":%.9e, "th":%.9e, "ph":%.9e, "tDot":%.9e, "rDot":%.9e, "thDot":%.9e, "phDot":%.9e, "x":%.9e, "y":%.9e, "z":%.9e}' \
+                 % (bl.mino, bl.tau, bl.e, bl.eR, bl.eTh, 10.0 * log10(bl.eCum if bl.eCum >= bl.nf else bl.nf), bl.t, bl.r, bl.th, bl.ph, bl.tDot / sigma, bl.vR / sigma, bl.vTh / sigma, bl.phDot / sigma, ra * bl.sth * cos(bl.ph), ra * bl.sth * sin(bl.ph), bl.r * bl.cth)  # Log data
         bl.update_t_phi()  # Euler's method
         bl.solve()  # update r and theta with symplectic integrator
 	if abs(bl.mino) > bl.T or bl.eCum > 1.0e-0:
--- genparam.py
+++ genparam.py
@@ -1,5 +1,18 @@
 #!/usr/bin/env python
+'''
+Copyright (c) 2014, 2015, Ian Smith (m4r35n357)
+All rights reserved.
 
+Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
+
+1. Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
+
+2. Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution.
+
+3. Neither the name of the copyright holder nor the names of its contributors may be used to endorse or promote products derived from this software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+'''
 from sys import argv, stdout, stderr
 from math import fabs, sin, cos, pi, sqrt
 import numpy as np
@@ -9,34 +22,28 @@ class InitialConditions(object):
     def __init__(self, particle, rMin, rMax, thetaMin, a, factorL, integrator):
         self.M = 1.0        
 	self.mu = 1.0 if (particle == True) else 0.0
-#       tolerance = 1.0e-6
-#	nonsingular = fabs(rMax - rMin) > 2.0 * tolerance;
-#	self.r0 = rMin if nonsingular else rMin - tolerance
-#	self.r1 = rMax if nonsingular else rMax + tolerance
-#	self.th0 = thetaMin if thetaMin > 0.01 else 0.01
+        self.mu2 = self.mu**2
 	self.r0 = rMin
 	self.r1 = rMax
-	self.th0 = pi - thetaMin 
+	self.th0 = pi * (1.0 - thetaMin) 
         self.a = a
+        self.a2 = self.a**2
 	self.factorL = factorL
 	self.integrator = integrator
         self.duration = 20.0
         self.timestep = 0.01
-        self.E = 1.0
-        self.L = 2.0
-        self.Q = 0.0
 
     def delta (self, r):
-        return r**2 - 2.0 * self.M * r + self.a**2
+        return r**2 - 2.0 * self.M * r + self.a2
 
     def PA (self, r, E, L):
-        return (r**2 + self.a**2) * E - self.a * L
+        return (r**2 + self.a2) * E - self.a * L
 
     def PB (self, r, E, L, Q):
-        return Q + (L - self.a * E)**2 + self.mu**2 * r**2
+        return Q + (L - self.a * E)**2 + self.mu2 * r**2
 
     def THETA (self, th, E, L, Q):
-        return Q - cos(th)**2 * (self.a**2 * (self.mu**2 - E**2) + L**2 / sin(th)**2)
+        return Q - cos(th)**2 * (self.a2 * (self.mu2 - E**2) + L**2 / sin(th)**2)
 
     def constantR (self, x):
         E = x[0]
@@ -45,7 +52,7 @@ class InitialConditions(object):
         return (self.PA(self.r0, E, L)**2 - self.delta(self.r0) * self.PB(self.r0, E, L, Q))**2 + \
                (4.0 * self.r0 * E * self.PA(self.r0, E, L) - \
                 2.0 * (self.r0 - self.M) * self.PB(self.r0, E, L, Q) - \
-                2.0 * self.mu**2 * self.r0 * self.delta(self.r0))**2 + \
+                2.0 * self.mu2 * self.r0 * self.delta(self.r0))**2 + \
                (self.THETA(self.th0, E, L, Q))**2
 
     def variableR (self, x):
@@ -66,17 +73,17 @@ class InitialConditions(object):
         
 def main ():
     if len(argv) == 6:
-        ic = InitialConditions(True, float(argv[1]), float(argv[2]), float(argv[3]) * pi, float(argv[4]), float(argv[5]), 8)
+        ic = InitialConditions(True, float(argv[1]), float(argv[2]), float(argv[3]), float(argv[4]), float(argv[5]), 8)
 	ic.solve(ic.variableR)
         rValue = 0.5 * (ic.r0 + ic.r1)
         thValue = 0.5 * pi
     elif len(argv) == 5:
-        ic = InitialConditions(True, float(argv[1]), 0.0, float(argv[2]) * pi, float(argv[3]), float(argv[4]), 8)
+        ic = InitialConditions(True, float(argv[1]), 0.0, float(argv[2]), float(argv[3]), float(argv[4]), 8)
 	ic.solve(ic.constantR)
         rValue = ic.r0
         thValue = 0.5 * pi
     elif len(argv) == 4:
-        ic = InitialConditions(True, float(argv[1]), 0.0, float(argv[2]) * pi, float(argv[3]), 0.0, 8)
+        ic = InitialConditions(True, float(argv[1]), 0.0, float(argv[2]), float(argv[3]), 0.0, 8)
 	ic.solve(ic.constantR)
         rValue = ic.r0
         thValue = ic.th0
@@ -95,7 +102,7 @@ def main ():
     print >> stdout, "  \"step\" : " + str(ic.timestep) + ","
     print >> stdout, "  \"integratorOrder\" : " + str(ic.integrator)
     print >> stdout, "}"
-    rscale = rValue + 10
+    rscale = rValue + 5.0
     thscale = 0.5 * pi
     nPoints = 1000
     for x in range(0, nPoints):
