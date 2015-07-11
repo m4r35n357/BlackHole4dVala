343979d0f5abcd72983b598946bacc07f2f706ee
Ian Smith <m4r35n357@gmail.com>
2015-07-11 13:29:55 +0100

    interpolate error chart

--- errorchart.py
+++ errorchart.py
@@ -4,13 +4,38 @@ from sys import argv
 from math import fabs, log10
 from matplotlib import pyplot
 from json import loads
+from array import array
+from scipy.interpolate import interp1d
+import numpy as np
 
 def main():
 	if len(argv) < 3:
-		raise Exception('>>> ERROR! Please supply a data file name and a plotting interval <<<')
+		raise Exception('>>> ERROR! Please supply a data file name and the number of points to plot <<<')
 	dataFile = open(argv[1], 'r')
-	interval = int(argv[2])
+	nPoints = int(argv[2])
 	line = dataFile.readline()
+	tau = array('d')
+	eR = array('d')
+	eTh = array('d')
+	ev4 = array('d')
+	tauMax = 0.0
+	while line:
+		p = loads(line)
+		tauValue = p['mino']
+		tauMax = tauValue if tauValue > tauMax else tauMax
+		tau.append(tauValue)
+		eR.append(p['ER'])
+		eTh.append(p['ETh'])
+		ev4.append(p['v4e'])
+		line = dataFile.readline()
+	# interpolate here
+        try:
+        	eRI = interp1d(tau, eR, kind='linear')
+		eThI = interp1d(tau, eTh, kind='linear')
+		ev4I = interp1d(tau, ev4, kind='linear')
+        except ValueError as e:
+            print('DATA ERROR: ' + str(argv[0]) + ': ' + str(e))
+            exit(-2)		
 	ax1 = pyplot.figure().add_subplot(111)
         pyplot.grid(b=True, which='major', color='k', linestyle='-')
 	ax1.set_xlabel('Mino Time, lambda')
@@ -19,22 +44,16 @@ def main():
 	ax2 = ax1.twinx()
 	ax2.set_ylabel('4-Velocity Norm Error', color='g')
         ax2.set_ylim(-180.0, 0.0)
-	n = 0
-	while line:
-		p = loads(line)
-		if (n % interval == 0):
-			ax1.plot(p['mino'], p['ETh'], 'r.', markersize=1)
-			ax1.plot(p['mino'], p['ER'], 'b.', markersize=1)
-#			ax2.plot(p['mino'], p['E'], 'k.', markersize=2)
-			ax2.plot(p['mino'], p['v4e'], 'g.', markersize=2)
-		line = dataFile.readline()
-		n += 1
-#        pyplot.legend(['v4e', 'Er', 'Eth'], loc='best')
+	tauI = np.linspace(0, tauMax, num = nPoints)
+	for i in range(len(tauI)):
+		ax1.plot(tauI[i], eRI(tauI[i]), 'b.', markersize=2)
+		ax1.plot(tauI[i], eThI(tauI[i]), 'r.', markersize=2)
+		ax2.plot(tauI[i], ev4I(tauI[i]), 'g.', markersize=2)
         try:
             pyplot.show()
         except AttributeError as e:
-            print('ATTRIBUTE ERROR: ' + str(argv[0]) + ': ' + str(e))
-            exit(-1)		
+            print('ATTRIBUTE ERROR: ' + str(argv[0]) + ':' + str(coordinate) + ': ' + str(e))
+            exit(-3)		
 
 if __name__ == "__main__":
 	main()
