ff8547517d34aa8bbb99ce0dae5eb821725def24
Ian Smith <m4r35n357@gmail.com>
2015-07-12 17:52:54 +0100

    refactoring, tweaking transparency

--- filegraphics.py
+++ filegraphics.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 
 from sys import argv, stderr, exit
-from math import sqrt, sin
+from math import sqrt, sin, fabs
 from visual import scene, sphere, curve, points, rate, ellipsoid, ring
 from json import loads
 from array import array
@@ -16,22 +16,12 @@ def isco (a):
 	else:
 		return 3.0 + z2 + sqrt((3.0 - z1) * (3.0 + z1 + 2.0 * z2))
 
-def drag (r, theta, a, m):
-	R2 = r ** 2 * a ** 2
-	DELTA = R2 - 2.0 * m * r
-	return 2.0 * a * m * r / (R2 ** 2 - a ** 2 * DELTA * sin(theta) ** 2) 
-
 def main():
 	if len(argv) < 4:
 		raise Exception('>>> ERROR! Please supply a data file name, a parameter file name, and the number of points to use <<<')
 	dataFile = open(argv[1], 'r')
 	parameterFile = open(argv[2], 'r')
 	nData = int(argv[3])
-	# scene basics
-	scene.center = (0,0,0)
-	scene.width = scene.height = 1000
-	scene.range = (20.0, 20.0, 20.0)
-	colours = [ (1.0, 1.0, 1.0), (1.0, 0.0, 0.0), (0.0, 1.0, 0.0), (0.0, 0.0, 1.0), (0.7, 0.7, 0.7), (0.5, 0.5, 0.0), (0.5, 0.0, 0.5), (0.0, 1.0, 1.0), (1.0, 1.0, 0.0), (0.0, 0.0, 0.0) ]
 	# get parameters
         try:
             parameters = loads(parameterFile.read())
@@ -42,7 +32,7 @@ def main():
 	m = parameters['M']
 	horizon = m * (1.0 + sqrt(1.0 - a * a));
 	cauchy = m * (1.0 - sqrt(1.0 - a * a));
-	# get data dimensions
+	# get raw data
         line = dataFile.readline()
 	tau = array('d')
 	x = array('d')
@@ -71,22 +61,24 @@ def main():
             exit(-2)		
 	tauI = np.linspace(0, tauMax, num = nData)
 	#  set up the scene
+	scene.center = (0.0, 0.0, 0.0)
+	scene.width = scene.height = 1000.0
+	scene.range = (20.0, 20.0, 20.0)
+	colours = [ (1.0, 1.0, 1.0), (1.0, 0.0, 0.0), (0.0, 1.0, 0.0), (0.0, 0.0, 1.0), (0.7, 0.7, 0.7), (0.5, 0.5, 0.0), (0.5, 0.0, 0.5), (0.0, 1.0, 1.0), (1.0, 1.0, 0.0), (0.0, 0.0, 0.0) ]
         inner = 2.0 * sqrt(cauchy**2 + a**2)
-	ellipsoid(pos = (0.0, 0.0, 0.0), length = inner, height = inner, width = 2.0 * cauchy, color = colours[3], opacity = 0.2)  # Inner Horizon
+	ellipsoid(pos = scene.center, length = inner, height = inner, width = 2.0 * cauchy, color = colours[3], opacity = 0.4)  # Inner Horizon
         outer = 2.0 * sqrt(horizon**2 + a**2)
-	ellipsoid(pos = (0.0, 0.0, 0.0), length = outer, height = outer, width = 2.0 * horizon, color = colours[7], opacity = 0.2)  # Outer Horizon
+	ellipsoid(pos = scene.center, length = outer, height = outer, width = 2.0 * horizon, color = colours[3], opacity = 0.3)  # Outer Horizon
         ergo = 2.0 * sqrt(4.0 + a**2)
-	ellipsoid(pos = (0.0, 0.0, 0.0), length = ergo, height = ergo, width = 2.0 * horizon, color = colours[0], opacity = 0.1)  # Ergosphere
-        if a > 0.0:
-	    ring(pos=(0.0, 0.0, 0.0), axis=(0, 0, 1), radius = a, color = colours[0], thickness=0.01)  # Singularity
+	ellipsoid(pos = scene.center, length = ergo, height = ergo, width = 2.0 * horizon, color = colours[4], opacity = 0.2)  # Ergosphere
+        if fabs(a) > 0.0:
+	    ring(pos=scene.center, axis=(0, 0, 1), radius = a, color = colours[0], thickness=0.01)  # Singularity
         else:
-            sphere(pos=(0.0, 0.0, 0.0), radius = 0.05, color = colours[0])  # Singularity
-	ring(pos=(0.0, 0.0, 0.0), axis=(0, 0, 1), radius = sqrt(isco(a)**2 + a**2), color = colours[6], thickness=0.03)  # ISCO
-#	hSpin = sphere(pos = (horizon, 0.0, 0.0), radius = 0.1, color = colours[1])  # Particle
-#	eSpin = sphere(pos = (2.0, 0.0, 0.0), radius = 0.1, color = colours[1])  # Particle
-#	iSpin = sphere(pos = (isco(a), 0.0, 0.0), radius = 0.1, color = colours[1])  # Particle
-	ball = sphere(pos = (xI(tauI[0]), yI(tauI[0]), zI(tauI[0])), radius = 0.2, color = (0.75, 0.75, 0.75))  # Particle
-	ball.trail = points(size = 1)
+            sphere(pos=scene.center, radius = 0.05, color = colours[0])  # Singularity
+	ring(pos=scene.center, axis=(0, 0, 1), radius = sqrt(isco(a)**2 + a**2), color = colours[6], thickness=0.01)  # ISCO
+	ball = sphere(pos = (xI(tauI[0]), yI(tauI[0]), zI(tauI[0])), radius = 0.2)  # Particle
+	ball.trail = curve(size = 1)  #  trail
+	# animate!
 	for i in range(len(tauI)):
 		rate(60)
 		error = eI(tauI[i])
@@ -98,6 +90,7 @@ def main():
 			colour = colours[8]
 		else:
 			colour = colours[1]
+		ball.color = colour
 		position = (xI(tauI[i]), yI(tauI[i]), zI(tauI[i]))
 		ball.pos = position
 		ball.trail.append(pos = position, color = colour, retain = 10000)
