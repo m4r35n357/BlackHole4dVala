5ad81269b2e21b0c159219968c69081dacb51dd8
Ian Smith <m4r35n357@gmail.com>
2015-07-16 22:51:35 +0100

    simplification

--- filegraphics.py
+++ filegraphics.py
@@ -77,13 +77,12 @@ def main():
             sphere(pos=scene.center, radius = 0.05, color = colours[0])  # Singularity
 	ring(pos=scene.center, axis=(0, 0, 1), radius = sqrt(isco(a)**2 + a**2), color = colours[6], thickness=0.01)  # ISCO
 	# animate!
-	ball = sphere(pos = (xI(tauI[0]), yI(tauI[0]), zI(tauI[0])), radius = 0.2)  # Particle
+	ball = sphere()  # Particle
 	counter = 0
 	for i in range(len(tauI)):
 		if counter % 1000 == 0:
 			ball.visible = False
-			del ball
-			ball = sphere(pos = (xI(tauI[0]), yI(tauI[0]), zI(tauI[0])), radius = 0.2)  # Particle
+			ball = sphere(radius = 0.2)  # Particle
 			ball.trail = curve(size = 1)  #  trail
 		rate(60)
 		error = eI(tauI[i])
