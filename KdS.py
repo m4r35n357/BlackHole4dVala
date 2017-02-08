m4r35n357/BlackHole4dVala

commit ae2ef112a90adbb88ec2e72960e08695c7630310

absorb step size into the symplectic coefficients

KdS.py
@@ -19,41 +19,43 @@ from sys import stdin, stderr, stdout, argv
 
 
 class Symplectic(object):
-    def __init__(self, model, order):
+    def __init__(self, model, h, order):
         if order == 'sb1':
+            self.coefficients = [h]
             self.step = self.symplectic_euler
         elif order == 'sb2':
+            self.coefficients = [0.5 * h, h]
             self.step = self.stormer_verlet
         elif order == 'sb4':
             self.cbrt2 = 2.0 ** (1.0 / 3.0)
-            self.f2 = 1.0 / (2.0 - self.cbrt2)
+            self.f2 = h / (2.0 - self.cbrt2)
             self.coefficients = [0.5 * self.f2, self.f2, 0.5 * (1.0 - self.cbrt2) * self.f2, - self.cbrt2 * self.f2]
             self.step = self.forest_ruth
         else:
             raise Exception('>>> Integrator must be sb1, sb2 or sb4, was "{}" <<<'.format(order))
         self.model = model
 
-    def symplectic_euler(self, w):
-        self.model.qUp(w)
-        self.model.pUp(w)
+    def symplectic_euler(self):
+        self.model.qUp(self.coefficients[0])
+        self.model.pUp(self.coefficients[0])
 
-    def stormer_verlet(self, w):
-        self.model.qUp(w * 0.5)
-        self.model.pUp(w)
-        self.model.qUp(w * 0.5)
+    def stormer_verlet(self):
+        self.model.qUp(self.coefficients[0])
+        self.model.pUp(self.coefficients[1])
+        self.model.qUp(self.coefficients[0])
 
-    def forest_ruth(self, w):
-        self.model.qUp(w * self.coefficients[0])
-        self.model.pUp(w * self.coefficients[1])
-        self.model.qUp(w * self.coefficients[2])
-        self.model.pUp(w * self.coefficients[3])
-        self.model.qUp(w * self.coefficients[2])
-        self.model.pUp(w * self.coefficients[1])
-        self.model.qUp(w * self.coefficients[0])
+    def forest_ruth(self):
+        self.model.qUp(self.coefficients[0])
+        self.model.pUp(self.coefficients[1])
+        self.model.qUp(self.coefficients[2])
+        self.model.qUp(self.coefficients[3])
+        self.model.pUp(self.coefficients[2])
+        self.model.qUp(self.coefficients[1])
+        self.model.qUp(self.coefficients[0])
 
 
 class BhSymp(object):
-    def __init__(self, Lambda, a, mu2, E, L, C, r0, th0, order):
+    def __init__(self, Lambda, a, mu2, E, L, C, r0, th0, h, order):
         self.l_3 = Lambda / 3.0
         self.a = a
         self.mu2 = mu2
@@ -70,7 +72,8 @@ class BhSymp(object):
         self.t = self.ph = 0.0
         self.r = r0
         self.th = (90.0 - th0) * pi / 180.0
-        self.integrator = Symplectic(self, order)
+        self.h = h
+        self.integrator = Symplectic(self, h, order)
 
     def refresh(self):
         self.r2 = self.r**2
@@ -123,7 +126,7 @@ class BhSymp(object):
         print >> stdout, '{"mino":%.9e, "tau":%.9e, "v4e":%.1f, "v4c":%.1f, "ER":%.1f, "ETh":%.1f, "t":%.9e, "r":%.9e, "th":%.9e, "ph":%.9e, "tP":%.9e, "rP":%.9e, "thP":%.9e, "phP":%.9e}' \
                 % (mino, tau, v4e, -180.0, eR, eTh, self.t, self.r, self.th, self.ph, self.Ut / self.S, self.Ur / self.S, self.Uth / self.S, self.Uph / self.S)  # Log data,  d/dTau = 1/sigma * d/dLambda !!!
 
-    def solve(self, start, end, h, tr):
+    def solve(self, start, end, tr):
         mino = tau = 0.0
         i = plotCount = 0
         self.refresh()
@@ -133,10 +136,10 @@ class BhSymp(object):
             if tau >= start and i % tr == 0:
                 self.plot(mino, tau)
                 plotCount += 1
-            self.integrator.step(h)
+            self.integrator.step()
             i += 1
-            mino = h * i
-            tau += h * self.S
+            mino = self.h * i
+            tau += self.h * self.S
         self.plot(mino, tau)
         return i, plotCount
 
@@ -145,5 +148,5 @@ print >> stderr, "Executable: {}".format(argv[0])
 input_data = stdin.read()
 ic = loads(input_data)['IC']
 print >> stderr, input_data
-BhSymp(ic['lambda'], ic['a'], ic['mu'], ic['E'], ic['L'], ic['Q'], ic['r0'], ic['th0'], ic['integrator']).solve(
-    ic['start'], ic['end'], ic['step'], ic['plotratio'])
+BhSymp(ic['lambda'], ic['a'], ic['mu'], ic['E'], ic['L'], ic['Q'], ic['r0'], ic['th0'], ic['step'], ic['integrator']).solve(
+    ic['start'], ic['end'], ic['plotratio'])

KdS.vala
@@ -28,9 +28,9 @@ namespace Simulations {
             if (o.has_member("Lfac")) {
                 new Newton(o.get_double_member("Lfac"),
                            o.get_double_member("r0"),
+                           o.get_double_member("step"),
                            o.get_string_member("integrator")).solve(o.get_double_member("start"),
                                                                     o.get_double_member("end"),
-                                                                    o.get_double_member("step"),
                                                                     o.get_int_member("plotratio"));
             } else if (o.has_member("a")) {
                 BhSymp.newInstance(o.get_double_member("lambda"),
@@ -41,9 +41,9 @@ namespace Simulations {
                                     o.get_double_member("Q"),
                                     o.get_double_member("r0"),
                                     o.get_double_member("th0"),
+                                    o.get_double_member("step"),
                                     o.get_string_member("integrator")).solve(o.get_double_member("start"),
                                                                              o.get_double_member("end"),
-                                                                             o.get_double_member("step"),
                                                                              o.get_int_member("plotratio"));
             } else if (o.has_member("bodies")) {
                 Body[] bodies = {};
@@ -72,9 +72,9 @@ namespace Simulations {
                 }
                 new NBody(bodies, o.get_double_member("g"),
                                   o.get_double_member("errorLimit"),
+                                  o.get_double_member("step"),
                                   o.get_string_member("integratorOrder")).solve(o.get_double_member("start"),
                                                                                 o.get_double_member("end"),
-                                                                                o.get_double_member("step"),
                                                                                 o.get_int_member("plotratio"));
             }
         } else if ("GenParticle" in executable) {

TestKdS.vala
@@ -20,7 +20,7 @@ namespace Simulations {
             var end = 100;
             var step = 0.00001;
             var interval = 10;
-            var counts = BhSymp.newInstance(0.0, 1.0, 1.0, 0.96210432940242041, 5.6843449527674236e-13, 15.914691393798241, 12.0, 0.0, "sc6").solve(start, end, step, interval);
+            var counts = BhSymp.newInstance(0.0, 1.0, 1.0, 0.96210432940242041, 5.6843449527674236e-13, 15.914691393798241, 12.0, 0.0, step, "sc6").solve(start, end, interval);
             assert(counts[0] == 69174);
             assert(counts[1] == 6918);
         });
@@ -32,7 +32,7 @@ namespace Simulations {
             var end = 100;
             var step = 0.0001;
             var interval = 10;
-            var counts = BhSymp.newInstance(0.0, 1.0, 0.0, 1.0, -2.0, 27.0, 3.0, 0.0, "sc6").solve(start, end, step, interval);
+            var counts = BhSymp.newInstance(0.0, 1.0, 0.0, 1.0, -2.0, 27.0, 3.0, 0.0, step, "sc6").solve(start, end, interval);
             assert(counts[0] == 105951);
             assert(counts[1] == 10596);
         });
@@ -44,7 +44,7 @@ namespace Simulations {
             var end = 100;
             var step = 0.00005;
             var interval = 10;
-            var counts = BhSymp.newInstance(0.0, 0.8, 1.0, 0.94550509567490792, 1.4343745095317371, 7.9787599589278697, 7.5, 0.0, "sb4").solve(start, end, step, interval);
+            var counts = BhSymp.newInstance(0.0, 0.8, 1.0, 0.94550509567490792, 1.4343745095317371, 7.9787599589278697, 7.5, 0.0, step, "sb4").solve(start, end, interval);
             assert(counts[0] == 85532);
             assert(counts[1] == 8554);
         });
@@ -56,7 +56,7 @@ namespace Simulations {
             var end = 100;
             var step = 0.00005;
             var interval = 10;
-            var counts = BhSymp.newInstance(0.0, 0.8, 1.0, 0.94550509567490792, 1.4343745095317371, 7.9787599589278697, 7.5, 0.0, "sb4").solve(start, end, step, interval);
+            var counts = BhSymp.newInstance(0.0, 0.8, 1.0, 0.94550509567490792, 1.4343745095317371, 7.9787599589278697, 7.5, 0.0, step, "sb4").solve(start, end, interval);
             assert(counts[0] == 85532);
             assert(counts[1] == 3215);
         });

bh3dSymp.vala
@@ -57,12 +57,13 @@ namespace Simulations {
         protected double Uph;
 
         // Constants from IC file
+        protected double h;
         private ISymplectic integrator;
 
         /**
          * Private constructor, use a static factory
          */
-        private BhSymp (double lambda, double a, double mu2, double E, double L, double Q, double r0, double th0, string type) {
+        private BhSymp (double lambda, double a, double mu2, double E, double L, double Q, double r0, double th0, double h, string type) {
             stderr.printf("Kerr-deSitter Geodesic\n");
             this.l_3 = lambda / 3.0;
             this.a = a;
@@ -78,7 +79,8 @@ namespace Simulations {
             this.K = Q + X2 * (L - aE) * (L - aE);
             this.r = r0;
             this.th = (90.0 - th0) * PI / 180.0;
-            this.integrator = Symplectic.getIntegrator(this, type);
+            this.h = h;
+            this.integrator = Symplectic.getIntegrator(this, h, type);
         }
 
         /**
@@ -122,7 +124,7 @@ namespace Simulations {
         /**
          * Externally visible method, sets up and controls the simulation
          */
-        public int[] solve (double start, double end, double h, int64 tr) {
+        public int[] solve (double start, double end, int64 tr) {
             var mino = 0.0;
             var tau = 0.0;
             var i = 0;
@@ -135,7 +137,7 @@ namespace Simulations {
                     plot(mino, tau);
                     plotCount += 1;
                 }
-                integrator.compose(h);
+                integrator.compose();
                 i += 1;
                 mino = h * i;
                 tau += h * S;
@@ -174,9 +176,9 @@ namespace Simulations {
         /**
          *  Static factory
          */
-        public static ISolver newInstance(double lambda, double a, double mu2, double E, double L, double Q, double r0, double th0, string t) {
+        public static ISolver newInstance(double lambda, double a, double mu2, double E, double L, double Q, double r0, double th0, double h , string t) {
             if (("sb1" == t) || ("sb2" == t) || ("sb4" == t) || ("sc4" == t) || ("sc6" == t) || ("sh6" == t) || ("sh8" == t) || ("sh10" == t)) {
-                return new BhSymp(lambda, a, mu2, E, L, Q, r0, th0, t);
+                return new BhSymp(lambda, a, mu2, E, L, Q, r0, th0, h, t);
             } else {
                 stderr.printf("Bad integrator; should be [ sb2 | sb4 | sc4 | sc6 | sh6 | sh8 | sh10 ], found {%s}\n", t);
                 assert_not_reached();

nbody3d.vala
@@ -47,15 +47,17 @@ namespace Simulations {
         private int np;
         private double g;
         private double errorLimit;
+        private double ts;
         private ISymplectic integrator;
 
-        public NBody (Body[] bodies, double g, double errorLimit, string type) {
+        public NBody (Body[] bodies, double g, double errorLimit, double h, string type) {
             stderr.printf("Newtonian N-Body Simulation\n");
             this.bodies = bodies;
             this.np = bodies.length;
             this.g = g;
             this.errorLimit = errorLimit;
-            this.integrator = Symplectic.getIntegrator(this, type);
+            this.ts = h;
+            this.integrator = Symplectic.getIntegrator(this, h, type);
         }
 
         /**
@@ -114,22 +116,22 @@ namespace Simulations {
         /**
          * Externally visible method, sets up and controls the simulation
          */
-        public int[] solve (double start, double end, double ts, int64 tr) {
+        public int[] solve (double start, double end, int64 tr) {
             var h0 = h();
-            var count = 0;
+            var i = 0;
             var t = 0.0;
             var dbValue = -180.0;
             while ((t < end) && (dbValue < errorLimit)) {
                 var hNow = h();
                 dbValue = logError(fabs(hNow / h0 - 1.0));
-                if ((t > start) && (count % tr == 0)) {
+                if ((t > start) && (i % tr == 0)) {
                     output(t, hNow, h0, dbValue);
                 }
-                integrator.compose(ts);
-                count += 1;
-                t = count * ts;
+                integrator.compose();
+                i += 1;
+                t = i * ts;
             }
-            return { count };
+            return { i };
         }
 
         /**

newton.vala
@@ -18,7 +18,6 @@ namespace Simulations {
 
     public class Newton : IModel, ISolver, GLib.Object {
 
-        private double E0;
         private double L;
         private double r;
         private double ph;
@@ -26,19 +25,21 @@ namespace Simulations {
         private double phDot;
         private double L2;
         private double H0;
+        private double h;
         private ISymplectic integrator;
 
-        public Newton (double lFac, double r0, string type) {
+        public Newton (double lFac, double r0, double h, string type) {
             stderr.printf("Newtonian Orbit\n");
             this.L = sqrt(r0);
             this.L2 = L * L;
             this.r = r0;
-            this.integrator = Symplectic.getIntegrator(this, type);
-            this.E0 = V(r);
+            var E0 = V(r);
             this.L = lFac * L;
             this.L2 = L * L;
             this.rDot = - sqrt(2.0 * fabs(E0 - V(r)));
             this.H0 = H();
+            this.h = h;
+            this.integrator = Symplectic.getIntegrator(this, h, type);
         }
 
         private double V (double r) {
@@ -65,19 +66,19 @@ namespace Simulations {
         /**
          * Externally visible method, sets up and controls the simulation
          */
-        public int[] solve (double start, double end, double h, int64 tr) {
-            var count = 0;
+        public int[] solve (double start, double end, int64 tr) {
+            var i = 0;
             var t = 0.0;
             while (t < end) {
-                if ((t > start) && (count % tr == 0)) {
+                if ((t > start) && (i % tr == 0)) {
                     output(t);
                 }
-                integrator.compose(h);
-                count += 1;
-                t = count * h;
+                integrator.compose();
+                i += 1;
+                t = i * h;
             }
             output(t);
-            return { count };
+            return { i };
         }
 
         /**

symplectic.vala
@@ -21,42 +21,45 @@ namespace Simulations {
      */
     protected abstract class Symplectic : ISymplectic, GLib.Object {
 
-        private double[] compositionWeights;
         protected IModel model;
+        private double h;
+        private double[] compositionWeights;
+        protected double[] coefficients;
 
         /**
          * Protected constructor, use the static factory
          */
-        protected Symplectic (IModel model, double[] compositionWeights) {
-            this.compositionWeights = compositionWeights;
+        protected Symplectic (IModel model, double h, double[] compositionWeights) {
             this.model = model;
+            this.h = h;
+            this.compositionWeights = compositionWeights;
         }
 
         /**
          * Static factory
          */
-        public static ISymplectic getIntegrator (IModel model, string type) {
+        public static ISymplectic getIntegrator (IModel model, double h, string type) {
             switch (type) {
                 case "sb1":
                     stderr.printf("1st Order Symplectic Integrator\n");
-                    return new EulerCromer(model, { 1.0 });
+                    return new EulerCromer(model, h, { 1.0 });
                 case "sb2":
                     stderr.printf("2nd Order Symplectic Integrator\n");
-                    return new StormerVerlet(model, { 1.0 });
+                    return new StormerVerlet(model, h, { 1.0 });
                 case "sb4":
                     stderr.printf("4th Order Symplectic Integrator\n");
-                    return new ForestRuth(model, { 1.0 });
+                    return new ForestRuth(model, h, { 1.0 });
                 case "sc4":
                     stderr.printf("4th Order Symplectic Integrator (Composed from 2nd order)\n");
                     var CUBRT2 = pow(2.0, 1.0/3.0);
-                    return new StormerVerlet(model, { 1.0/(2.0-CUBRT2), -CUBRT2/(2.0-CUBRT2), 1.0/(2.0-CUBRT2) });
+                    return new StormerVerlet(model, h, { 1.0/(2.0-CUBRT2), -CUBRT2/(2.0-CUBRT2), 1.0/(2.0-CUBRT2) });
                 case "sc6":
                     stderr.printf("6th Order Symplectic Integrator (Composed from 4th order)\n");
                     var FTHRT2 = pow(2.0, 1.0/5.0);
-                    return new ForestRuth(model, { 1.0/(2.0-FTHRT2), -FTHRT2/(2.0-FTHRT2), 1.0/(2.0-FTHRT2) });
+                    return new ForestRuth(model, h, { 1.0/(2.0-FTHRT2), -FTHRT2/(2.0-FTHRT2), 1.0/(2.0-FTHRT2) });
                 case "sh6":
                     stderr.printf("6th Order Symplectic Integrator (Composed from 2nd order)\n");
-                    return new StormerVerlet(model, {
+                    return new StormerVerlet(model, h, {
                                                 0.78451361047755726381949763,
                                                 0.23557321335935813368479318,
                                                 -1.17767998417887100694641568,
@@ -67,7 +70,7 @@ namespace Simulations {
                                             });
                 case "sh8":
                     stderr.printf("8th Order Symplectic Integrator (Composed from 2nd order)\n");
-                    return new StormerVerlet(model, {
+                    return new StormerVerlet(model, h, {
                                                 0.74167036435061295344822780,
                                                 -0.40910082580003159399730010,
                                                 0.19075471029623837995387626,
@@ -86,7 +89,7 @@ namespace Simulations {
                                             });
                 case "sh10":
                     stderr.printf("10th Order Symplectic Integrator (Composed from 2nd order)\n");
-                    return new StormerVerlet(model, {
+                    return new StormerVerlet(model, h, {
                                                 0.09040619368607278492161150,
                                                 0.53591815953030120213784983,
                                                 0.35123257547493978187517736,
@@ -134,9 +137,9 @@ namespace Simulations {
         /**
          * Perform a composition of weighted integration steps
          */
-        public void compose (double h) {
+        public void compose () {
             for (int i = 0; i < compositionWeights.length; i += 1) {
-                integrate(compositionWeights[i] * h);
+                integrate(compositionWeights[i]);
             }
         }
     }
@@ -149,16 +152,17 @@ namespace Simulations {
         /**
          * Protected constructor, use the static factory in superclass
          */
-        protected EulerCromer (IModel model, double[] compositionWeights) {
-            base(model, compositionWeights);
+        protected EulerCromer (IModel model, double h, double[] compositionWeights) {
+            base(model, h, compositionWeights);
+            this.coefficients = { h };
         }
 
         /**
          * Weighted 1st order integration step
          */
         protected override void integrate (double weight) {
-            model.qUp(weight);
-            model.pUp(weight);
+            model.qUp(coefficients[0] * weight);
+            model.pUp(coefficients[0] * weight);
         }
     }
 
@@ -170,17 +174,18 @@ namespace Simulations {
         /**
          * Protected constructor, use the static factory in superclass
          */
-        protected StormerVerlet (IModel model, double[] compositionWeights) {
-            base(model, compositionWeights);
+        protected StormerVerlet (IModel model, double h, double[] compositionWeights) {
+            base(model, h, compositionWeights);
+            this.coefficients = { 0.5 * h, h };
         }
 
         /**
          * Weighted 2nd order integration step
          */
         protected override void integrate (double weight) {
-            model.qUp(weight * 0.5);
-            model.pUp(weight);
-            model.qUp(weight * 0.5);
+            model.qUp(coefficients[0] * weight);
+            model.pUp(coefficients[1] * weight);
+            model.qUp(coefficients[0] * weight);
         }
     }
 
@@ -189,15 +194,13 @@ namespace Simulations {
      */
     public class ForestRuth : Symplectic {
 
-        protected double[] coefficients;
-
         /**
          * Protected constructor, use the static factory in superclass
          */
-        protected ForestRuth (IModel model, double[] compositionWeights) {
-            base(model, compositionWeights);
+        protected ForestRuth (IModel model, double h, double[] compositionWeights) {
+            base(model, h, compositionWeights);
             var CUBRT2 = pow(2.0, (1.0 / 3.0));
-            this.coefficients = { 0.5/(2.0-CUBRT2), 1.0/(2.0-CUBRT2), 0.5*(1.0-CUBRT2)/(2.0-CUBRT2), -CUBRT2/(2.0-CUBRT2) };
+            this.coefficients = { 0.5*h/(2.0-CUBRT2), h/(2.0-CUBRT2), 0.5*h*(1.0-CUBRT2)/(2.0-CUBRT2), -h*CUBRT2/(2.0-CUBRT2) };
         }
 
         /**

utils.vala
@@ -39,7 +39,7 @@ namespace Simulations {
         /**
          * Sole method called by main(), calls iterate() on RK4, and ISymplectic.compose() on the Symplectics
          */
-        public abstract int[] solve (double start, double end, double h, int64 tr);
+        public abstract int[] solve (double start, double end, int64 tr);
     }
 
     /**
@@ -64,7 +64,7 @@ namespace Simulations {
         /**
          * Should be called by IModel.solve() as needed, calls IModel.pUp() and IModel.qUp()
          */
-        public abstract void compose (double h);
+        public abstract void compose ();
     }
 
     /**

commit c10c77bf668d752b84e5663fc03966925d123e01

ae2ef112a90adbb88ec2e72960e08695c7630310 broke Python sb4, now fixed

KdS.py
@@ -48,9 +48,9 @@ class Symplectic(object):
         self.model.qUp(self.coefficients[0])
         self.model.pUp(self.coefficients[1])
         self.model.qUp(self.coefficients[2])
-        self.model.qUp(self.coefficients[3])
-        self.model.pUp(self.coefficients[2])
-        self.model.qUp(self.coefficients[1])
+        self.model.pUp(self.coefficients[3])
+        self.model.qUp(self.coefficients[2])
+        self.model.pUp(self.coefficients[1])
         self.model.qUp(self.coefficients[0])
 
 

icgenParticle-data.example
@@ -7,7 +7,11 @@
   "E0": 1.0,
   "L0": 5.0,
   "Q0": 0.0,
-  "integrator": "rk4",
+  "start" : 0.0,
+  "end" : 5000.0,
+  "step" : 0.001,
+  "plotratio" : 10,
+  "integrator": "sb2",
   "method": "dnewton",
   "maxIterations": 1000,
   "termination": "deltas",

rg2
@@ -9,6 +9,9 @@ NC='\033[0m' # No Color
 # Pretty-print the parameter file
 jq . $ic
 
+# Display the executable
+echo $exe
+
 # Run the simulator
 echo -n ${C}"Simulating with $ic . . . "
 $icgen <$ic >$pot 2>/dev/null