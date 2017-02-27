m4r35n357/BlackHole4dVala

commit d6c3893025aa1eb8b3a78bb3b706b511774f9a20

scale potential errors properly for tau

bh3dSymp.vala
@@ -134,7 +134,7 @@ namespace Simulations {
             this.Uth = - sqrt(TH >= 0.0 ? TH : -TH);
             while (tau < end) {
                 if ((tau >= start) && (i % tr == 0)) {
-                    plot(mino, tau);
+                    plot(mino, tau, Ut / S, Ur / S, Uth / S, Uph / S);
                     plotCount += 1;
                 }
                 integrator.integrate();
@@ -142,17 +142,18 @@ namespace Simulations {
                 mino = h * i;
                 tau += h * S;
             }
-            plot(mino, tau);
+            plot(mino, tau, Ut / S, Ur / S, Uth / S, Uph / S);
             return { i, plotCount };  // for testing
         }
 
         /**
          * Write the simulated data to STDOUT
          */
-        private void plot (double mino, double tau) {
-            var eR = 0.5 * (Ur * Ur - R);
-            var eTh = 0.5 * (Uth * Uth - TH);
-            var v4e = v4Error(Ut / S, Ur / S, Uth / S, Uph / S);
+        private void plot (double mino, double tau, double Ut, double Ur, double Uth, double Uph) {
+            var S2 = S * S;
+            var eR = 0.5 * (Ur * Ur - R / S2);
+            var eTh = 0.5 * (Uth * Uth - TH / S2);
+            var v4e = v4Error(Ut, Ur, Uth, Uph);
             stdout.printf("{\"mino\":%.9e,\"tau\":%.9e,\"v4e\":%.9e,\"ER\":%.9e,\"ETh\":%.9e,\"D_r\":%.9e,\"t\":%.9e,\"r\":%.9e,\"th\":%.9e,\"ph\":%.9e,\"tP\":%.9e,\"rP\":%.9e,\"thP\":%.9e,\"phP\":%.9e}\n", mino,tau, v4e,eR,eTh,D_r, t,r,th,ph, Ut/S,Ur/S,Uth/S,Uph/S);
         }
 

KdS.py
@@ -110,12 +110,12 @@ class BhSymp(object):
         self.Uth += h * (cos(self.th) * (self.sth * self.a2 * (self.mu2 * self.D_th - self.l_3 * (self.K - self.a2mu2 * self.cth2))
                         + self.X2 * self.T / self.sth * (self.T / self.sth2 - 2.0 * self.aE)))
 
-    def plot(self, mino, tau):
-        eR = 0.5 * (self.Ur**2 - self.R)
-        eTh = 0.5 * (self.Uth**2 - self.TH)
-        v4e =self.v4_error(self.Ut / self.S, self.Ur / self.S, self.Uth / self.S, self.Uph / self.S)
-        print >> stdout, '{"mino":%.9e,"tau":%.9e,"v4e":%.9e,"ER":%.9e,"ETh":%.9e,"t":%.9e,"r":%.9e,"th":%.9e,"ph":%.9e,"tP":%.9e,"rP":%.9e,"thP":%.9e,"phP":%.9e}' \
-                % (mino, tau, v4e, eR, eTh, self.t, self.r, self.th, self.ph, self.Ut / self.S, self.Ur / self.S, self.Uth / self.S, self.Uph / self.S)  # Log data,  d/dTau = 1/sigma * d/dLambda !!!
+    def plot(self, mino, tau, Ut ,Ur, Uth, Uph):
+        eR = 0.5 * (Ur**2 - self.R / self.S**2)
+        eTh = 0.5 * (Uth**2 - self.TH / self.S**2)
+        v4e =self.v4_error(Ut, Ur, Uth, Uph)
+        print >> stdout, '{"mino":%.9e,"tau":%.9e,"v4e":%.9e,"ER":%.9e,"ETh":%.9e,"t":%.9e,"r":%.9e,"th":%.9e,"ph":%.9e,"tP":%.9e,"rP":%.9e,"thP":%.9e,"phP":%.9e}' % (mino, tau, v4e, eR, eTh, self.t, self.r, self.th, self.ph, Ut, Ur, Uth, Uph)
+
 
     def solve(self, start, end, tr):
         mino = tau = 0.0
@@ -125,13 +125,13 @@ class BhSymp(object):
         self.Uth = - sqrt(self.TH if self.TH >= 0.0 else -self.TH)
         while tau < end:
             if tau >= start and i % tr == 0:
-                self.plot(mino, tau)
+                self.plot(mino, tau, self.Ut / self.S, self.Ur / self.S, self.Uth / self.S, self.Uph / self.S)
                 plotCount += 1
             self.integrator.step()
             i += 1
             mino = self.h * i
             tau += self.h * self.S
-        self.plot(mino, tau)
+        self.plot(mino, tau, self.Ut / self.S, self.Ur / self.S, self.Uth / self.S, self.Uph / self.S)
         return i, plotCount
 
 

commit bdaac89733600313c88a9298190a7d33eb5f73b8

fix output velocity components, broken since d6c3893025aa1eb8b3a78bb3b706b511774f9a20

bh3dSymp.vala
@@ -152,7 +152,7 @@ namespace Simulations {
             var eR = 0.5 * (Ur * Ur - R / S2);
             var eTh = 0.5 * (Uth * Uth - TH / S2);
             var v4e = v4Error(Ut, Ur, Uth, Uph);
-            stdout.printf("{\"mino\":%.9e,\"tau\":%.9e,\"v4e\":%.9e,\"ER\":%.9e,\"ETh\":%.9e,\"t\":%.9e,\"r\":%.9e,\"th\":%.9e,\"ph\":%.9e,\"tP\":%.9e,\"rP\":%.9e,\"thP\":%.9e,\"phP\":%.9e}\n", mino,tau, v4e,eR,eTh, t,r,th,ph, Ut/S,Ur/S,Uth/S,Uph/S);
+            stdout.printf("{\"mino\":%.9e,\"tau\":%.9e,\"v4e\":%.9e,\"ER\":%.9e,\"ETh\":%.9e,\"t\":%.9e,\"r\":%.9e,\"th\":%.9e,\"ph\":%.9e,\"tP\":%.9e,\"rP\":%.9e,\"thP\":%.9e,\"phP\":%.9e}\n", mino,tau, v4e,eR,eTh, t,r,th,ph, Ut,Ur,Uth,Uph);
         }
     }
 }