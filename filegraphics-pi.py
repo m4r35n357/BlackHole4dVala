4c01dc7907fe0bc34e202a675e3a7ed71b38c6e5
Ian Smith <m4r35n357@gmail.com>
2017-11-12 18:47:21 +0000

    more pi3d refactoring

--- filegraphics-pi.py
+++ filegraphics-pi.py
@@ -4,120 +4,131 @@ from __future__ import absolute_import, division, print_function, unicode_litera
 
 from json import loads
 from numpy import sqrt, sin, cos, radians, array
-from sys import argv, stdin
+from sys import argv, stdin, stderr
 
 from pi3d import Sphere, Points, Display, Camera, Shader, Keyboard, screenshot
 
-
-def error_colour (e):
-    error = abs(e)
-    if error < 1.0e-12:
+def error_colour (error):
+    e = abs(error)
+    if e < 1.0e-12:
         return 0.0, 1.0, 0.0
-    elif error < 1.0e-9:
+    elif e < 1.0e-9:
         return 0.0, 0.5, 0.5
-    elif error < 1.0e-6:
+    elif e < 1.0e-6:
         return 1.0, 1.0, 0.0
-    elif error < 1.0e-3:
+    elif e < 1.0e-3:
         return 0.5, 0.5, 0.0
     else:
         return 1.0, 0.0, 0.0
 
-
-class Planet(Sphere):
-    def __init__(self, shdr, clr, radius, pos=(0.0, 0.0, 0.0), track_shader=None):
-        super(Planet, self).__init__(radius=radius, x=pos[0], y=pos[1], z=pos[2])
-        super(Planet, self).set_draw_details(shdr, [])
-        self.pos = array(pos)
+class Body(Sphere):
+    def __init__(self, shader, colour, radius, position=(0.0, 0.0, 0.0), track_shader=None):
+        super(Body, self).__init__(radius=radius, x=position[0], y=position[1], z=position[2])
+        super(Body, self).set_draw_details(shader, [])
+        self.pos = array(position)
         self.track_shader = track_shader
-        self.t_v = []
-        self.t_len = 0
+        self.vertices = []
+        self.track_length = 0
+        self.track_max = 2400
         self.trace_shape = None
-        self.set_material(clr)
+        self.set_material(colour)
 
     def position_and_draw(self, trace_material=(0.5, 0.5, 0.5)):
+        # body
         self.position(self.pos[0], self.pos[1], self.pos[2])
         self.draw()
-        if self.track_shader is not None:
-            self.t_len += 1
-            self.t_v.append(tuple(self.pos))
-            if (self.t_len % 10) == 0:
-                self.trace_shape = Points(vertices=self.t_v, material=trace_material)
+        # track
+        if self.track_shader:
+            self.track_length += 1
+            self.vertices.append(tuple(self.pos))
+            if (self.track_length % 10) == 0:
+                self.trace_shape = Points(vertices=self.vertices, material=trace_material)
                 self.trace_shape.set_shader(self.track_shader)
-                if self.t_len > 2400:
-                    self.t_v = self.t_v[-2400:]
-                    self.t_len = 2400
+                if self.track_length > self.track_max:
+                    self.vertices = self.vertices[-self.track_max:]
+                    self.track_length = self.track_max
             if self.trace_shape:
                 self.trace_shape.draw()
 
-
-if len(argv) < 2:
-    raise Exception('>>> ERROR! Please supply a parameter file name <<<')
-parameters = loads(open(argv[1]).read())
-interval = parameters['IC']['plotratio']
-m = parameters['IC']['M']
-a = parameters['IC']['a']
-a2 = a**2
-horizon = m * (1.0 + sqrt(1.0 - a2))
-# Setup display and initialise pi3d ------
-DISPLAY = Display.create(x=0, y=0, frames_per_second=0)
-DISPLAY.set_background(0, 0, 0, 1)  # r,g,b,alpha
-# Camera ---------------------------------
-CAMERA = Camera()
-rot = tilt = 0
-rot_tilt = True
-camRad = 30.0
-# Shaders --------------------------------
-shader = Shader("mat_light")
-tracksh = Shader("mat_flat")
-# Planets --------------------------------
-sun = Planet(shader, (0.0, 0.0, 1.0), horizon, pos=[0.0, 0.0, 0.0])
-earth = Planet(shader, (0.0, 1.0, 0.0), 0.125, track_shader=tracksh)
-# Fetch key presses ----------------------
-mykeys = Keyboard()
-# Display scene
-colour = None
-counter = 0
-line = stdin.readline()
-while DISPLAY.loop_running():
-    params = loads(line)
-    if rot_tilt:
-        CAMERA.reset()
-        CAMERA.rotate(-tilt, rot, 0)
-        CAMERA.position((camRad * sin(radians(rot)) * cos(radians(tilt)), camRad * sin(radians(tilt)),
-                         -camRad * cos(radians(rot)) * cos(radians(tilt))))
-        rot_tilt = False
-    sun.position_and_draw()
-    if counter % interval == 0:
-        r = float(params['r'])
-        th = float(params['th'])
-        ph = float(params['ph'])
-        ra_sth = sqrt(r**2 + a2) * sin(th)
-        earth.pos = [ra_sth * cos(ph), ra_sth * sin(ph), r * cos(th)]
-        colour = error_colour( params['v4e'])
-        earth.set_material(colour)
-    earth.position_and_draw(trace_material=colour)
-    k = mykeys.read()
-    if k > -1:
-        rot_tilt = True
-        if k == 112:
-            screenshot("orbit.jpg")
-        elif k == 119:  # key W rotate camera up
-            tilt += 2.0
-        elif k == 115:  # kry S down
-            tilt -= 2.0
-        elif k == 97:  # key A left
-            rot -= 2
-        elif k == 100:  # key D right
-            rot += 2
-        elif k == 61:  # key += in
-            camRad -= 0.5
-        elif k == 45:  # key _- out
-            camRad += 0.5
-        elif k == 27:
-            mykeys.close()
-            DISPLAY.destroy()
-            break
+def main():
+    print("pi3d Geodesic Plotter: {}".format(argv))
+    if len(argv) < 2:
+        raise Exception('>>> ERROR! Please supply a parameter file name <<<')
+    parameters = loads(open(argv[1]).read())['IC']
+    interval = parameters['plotratio']
+    m = parameters['M']
+    a = parameters['a']
+    a2 = a**2
+    # Setup display and initialise pi3d
+    display = Display.create(x=0, y=0, frames_per_second=0)
+    display.set_background(0, 0, 0, 1)  # r,g,b,alpha
+    # Camera
+    camera = Camera()
+    rot = tilt = 0
+    rot_tilt = True
+    cam_rad = 30.0
+    # Bodies
+    body_shader = Shader("mat_light")
+    black_hole = Body(body_shader, (0.0, 0.0, 1.0), m * (1.0 + sqrt(1.0 - a2)), position=[0.0, 0.0, 0.0])
+    particle = Body(body_shader, (0.0, 1.0, 0.0), 0.125, track_shader=Shader("mat_flat"))
+    # Enable key presses
+    keys = Keyboard()
+    # Display scene
+    counter = 1
+    cumulative_error = 0.0
     line = stdin.readline()
-    counter += 1
-    if not line:
-        DISPLAY.stop()
+    while display.loop_running():
+        data = loads(line)
+        # monitor errors
+        current_error = data['v4e']
+        cumulative_error += current_error if current_error >= 0.0 else -current_error
+        # camera control
+        if rot_tilt:
+            camera.reset()
+            camera.rotate(-tilt, rot, 0)
+            camera.position((cam_rad * sin(radians(rot)) * cos(radians(tilt)), cam_rad * sin(radians(tilt)),
+                             -cam_rad * cos(radians(rot)) * cos(radians(tilt))))
+            rot_tilt = False
+        # plot the black hole
+        black_hole.position_and_draw()
+        # plot the orbiter
+        if counter % interval == 0:
+            r = float(data['r'])
+            th = float(data['th'])
+            ph = float(data['ph'])
+            ra_sth = sqrt(r**2 + a2) * sin(th)
+            particle.pos = [ra_sth * cos(ph), ra_sth * sin(ph), r * cos(th)]
+            particle.set_material(error_colour(data['v4e']))
+            particle.position_and_draw(trace_material=error_colour(cumulative_error / counter))
+        # process keyboard input
+        key = keys.read()
+        if key > -1:
+            rot_tilt = True
+            if key == 112:
+                screenshot("orbit.jpg")
+            elif key == 119:  # key W rotate camera up
+                tilt += 2.0
+            elif key == 115:  # kry S down
+                tilt -= 2.0
+            elif key == 97:  # key A left
+                rot -= 2
+            elif key == 100:  # key D right
+                rot += 2
+            elif key == 61:  # key += in
+                cam_rad -= 0.5
+            elif key == 45:  # key _- out
+                cam_rad += 0.5
+            elif key == 27:
+                keys.close()
+                display.destroy()
+                break
+        # prepare for next iteration
+        line = stdin.readline()
+        counter += 1
+        if not line:
+            display.stop()
+
+if __name__ == "__main__":
+    main()
+else:
+    print(__name__ + " module loaded", file=stderr)
