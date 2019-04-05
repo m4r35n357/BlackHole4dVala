420ec51a5bf55d389b1b499f9719eeb95dd0e749
Ian Smith <m4r35n357@gmail.com>
2019-04-05 11:49:17 +0100

    refactoring

--- NelderMead.py
+++ NelderMead.py
@@ -1,107 +1,109 @@
+#!/usr/bin/env python3
 
 import copy
 from json import loads
-from sys import argv, stdin, stderr
-# import numpy as np
+from sys import argv, stderr
 from gmpy2 import mpfr, get_context, sin, acos, cos
 get_context().precision = 113  # Set this BEFORE importing any Taylor Series stuff!
 from Symplectic import D1, D2
 from dual import Dual, make_mpfr
 
 
-def nelder_mead(f, x_start,
-                step=0.1, no_improve_thr=10e-12,
-                no_improv_break=10, max_iter=0,
-                alpha=1., gamma=2., rho=-0.5, sigma=0.5):
+def nelder_mead(f, x_0, x_δ, stuck_threshold=10e-12, stuck_break=10, max_iterations=0, α=1.0, γ=2.0, ρ=-0.5, σ=0.5):
 
-    # init
-    dim = len(x_start)
-    prev_best = f(x_start)
+    # initial simplex
+    nt = nr = ne = nc = ns = 0
+    dim = len(x_0)
+    prev_best = f(x_0)
     no_improvement = 0
-    res = [[x_start, prev_best]]
-
+    results = [[x_0, prev_best]]
     for i in range(dim):
-        x = copy.copy(x_start)
-        x[i] = x[i] + step
+        x = copy.copy(x_0)
+        x[i] += x_δ[i]
         score = f(x)
-        res.append([x, score])
+        results.append([x, score])
 
-    # simplex iter
-    iters = 0
-    while 1:
-        # order
-        res.sort(key=lambda x: x[1])
-        best = res[0][1]
+    iterations = 0
+    while True:
+        nt += 1
+
+        # 1. order
+        results.sort(key=lambda z: z[1])
+        best = results[0][1]
 
         # break after max_iter
-        if max_iter and iters >= max_iter:
-            return res[0]
-        iters += 1
+        if max_iterations and iterations >= max_iterations:
+            return results[0], nt - 1, nr, ne, nc, ns
+        iterations += 1
 
         # break after no_improvement iterations with no improvement
         # print('...best so far:', best)
-
-        if best < prev_best - no_improve_thr:
+        if best < prev_best - stuck_threshold:
             no_improvement = 0
             prev_best = best
         else:
             no_improvement += 1
+        if no_improvement >= stuck_break:
+            return results[0], nt - 1, nr, ne, nc, ns
 
-        if no_improvement >= no_improv_break:
-            return res[0]
-
-        # centroid
-        x0 = [0.] * dim
-        for tup in res[:-1]:
+        # 2. centroid
+        x0 = [0.0] * dim
+        for tup in results[:-1]:
             for i, c in enumerate(tup[0]):
-                x0[i] += c / (len(res)-1)
+                x0[i] += c / (len(results)-1)
 
-        # reflection
-        worst = res[-1][0]
-        xr = [0.0, 0.0, 0.0]
+        # 3. reflect
+        worst = results[-1][0]
+        xr = [0.0] * dim
         for i in range(dim):
-            xr[i] = x0[i] + alpha*(x0[i] - worst[i])
-        rscore = f(xr)
-        if res[0][1] <= rscore < res[-2][1]:
-            del res[-1]
-            res.append([xr, rscore])
+            xr[i] = x0[i] + α * (x0[i] - worst[i])
+        r_score = f(xr)
+        if results[0][1] <= r_score < results[-2][1]:
+            nr += 1
+            del results[-1]
+            results.append([xr, r_score])
             continue
 
-        # expansion
-        if rscore < res[0][1]:
-            worst = res[-1][0]
-            xe = [0.0, 0.0, 0.0]
+        # 4. expand
+        if r_score < results[0][1]:
+            ne += 1
+            worst = results[-1][0]
+            xe = [0.0] * dim
             for i in range(dim):
-                xe[i] = x0[i] + gamma * (x0[i] - worst[i])
-            escore = f(xe)
-            if escore < rscore:
-                del res[-1]
-                res.append([xe, escore])
+                xe[i] = x0[i] + γ * (x0[i] - worst[i])
+            e_score = f(xe)
+            if e_score < r_score:
+                del results[-1]
+                results.append([xe, e_score])
                 continue
             else:
-                del res[-1]
-                res.append([xr, rscore])
+                del results[-1]
+                results.append([xr, r_score])
                 continue
 
-        # contraction
-        worst = res[-1][0]
-        xc = [0.0, 0.0, 0.0]
+        # 5. contract
+        worst = results[-1][0]
+        xc = [0.0] * dim
         for i in range(dim):
-            xc[i] = x0[i] + rho*(x0[i] - worst[i])
-        cscore = f(xc)
-        if cscore < res[-1][1]:
-            del res[-1]
-            res.append([xc, cscore])
+            xc[i] = x0[i] + ρ * (x0[i] - worst[i])
+        c_score = f(xc)
+        if c_score < results[-1][1]:
+            nc += 1
+            del results[-1]
+            results.append([xc, c_score])
             continue
 
-        # reduction
-        x1 = res[0][0]
-        nres = []
-        for tup in res:
-            redx = x1 + sigma*(tup[0] - x1)
-            score = f(redx)
-            nres.append([redx, score])
-        res = nres
+        # 6. shrink
+        ns += 1
+        x1 = results[0][0]
+        reduced = []
+        xs = [0.0] * dim
+        for tup in results:
+            for i in range(dim):
+                xs[i] = x1[i] + σ * (tup[0][i] - x1[i])
+            score = f(xs)
+            reduced.append([xs, score])
+        results = reduced
 
 
 class Potentials(object):
@@ -113,13 +115,13 @@ class Potentials(object):
         self.r_max = make_mpfr(r_max)
         self.θ = make_mpfr((D1 - (make_mpfr(90) - elevation) / make_mpfr(180)) * acos(make_mpfr(-1)))
 
-    def f_r(self, r, E, L, Q):
+    def f_r(self, r, e, l, q):
         r2 =r.sqr
         ra2 = r2 + self.a2
-        return (ra2 * E - self.a * L).sqr - (ra2 - D2 * r) * (self.μ2 * r2 + Q + (L - self.a * E)**2)
+        return (ra2 * e - self.a * l).sqr - (ra2 - D2 * r) * (self.μ2 * r2 + q + (l - self.a * e)**2)
 
-    def f_θ(self, θ, E, L, Q):
-        return Q - cos(θ)**2 * (self.a2 * (self.μ2 - E**2) + (L / sin(θ))**2)
+    def f_θ(self, θ, e, l, q):
+        return q - cos(θ)**2 * (self.a2 * (self.μ2 - e**2) + (l / sin(θ))**2)
 
     def f_spherical(self, x):
         E, L, Q = x
@@ -134,20 +136,16 @@ class Potentials(object):
 
 
 if __name__ == "__main__":
+    # Example: ./NelderMead.py icgen-data.json 1.0 5.0 0.0 1.0 1.0 1.0
     print("Generator: {}".format(argv[0]), file=stderr)
-    if argv[1]:
-        input_data = open(argv[1]).read()
-    else:
-        input_data = stdin.read()
+    input_data = open(argv[1]).read()
     ic = loads(input_data, parse_float=mpfr)
     print(input_data, file=stderr)
     if ic.get('rMax'):
         potentials = Potentials(ic['spin'], ic['rMin'], ic['rMax'], ic['elevation'])
-        f = potentials.f_nonspherical
-        print('NONSPHERICAL')
+        func = potentials.f_nonspherical
     else:
         potentials = Potentials(ic['spin'], ic['r'], ic['r'], ic['elevation'])
-        f = potentials.f_spherical
-        print('SPHERICAL')
-    print(nelder_mead(f, [0.9, 1.5, 8.0]))
+        func = potentials.f_spherical
+    print(nelder_mead(func, [make_mpfr(argv[2]), make_mpfr(argv[3]), make_mpfr(argv[4])], [make_mpfr(argv[5]), make_mpfr(argv[6]), make_mpfr(argv[7])]))
 
