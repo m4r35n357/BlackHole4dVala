c8c0a561067d481e8cf8af98a7d02e8b5a4600cc
Ian Smith <m4r35n357@gmail.com>
2019-04-06 11:00:35 +0100

    simplifying

--- NelderMead.py
+++ NelderMead.py
@@ -1,20 +1,24 @@
 from copy import copy
 from sys import stderr
 
+def replace_worst(data, new):
+    del data[-1]
+    data.append(new)
+
 def nelder_mead(f, x_0, x_δ, stuck_threshold=10e-12, stuck_break=10, max_iterations=0, α=1.0, γ=2.0, ρ=-0.5, σ=0.5):
 
     # initial simplex
     dim = len(x_0)
     assert dim == len(x_δ)
+    dimensions = range(dim)
     prev_best = f(x_0)
-    no_improvement = 0
     results = [[x_0, prev_best]]
-    for i in range(dim):
+    for i in dimensions:
         x = copy(x_0)
         x[i] += x_δ[i]
         score = f(x)
         results.append([x, score])
-    iterations = nt = nr = ne = nc = ns = 0
+    iterations = no_improvement = nt = nr = ne = nc = ns = 0
 
     while True:
         nt += 1
@@ -40,60 +44,41 @@ def nelder_mead(f, x_0, x_δ, stuck_threshold=10e-12, stuck_break=10, max_iterat
 
         # 2. centroid
         x0 = [0.0] * dim
-        for tup in results[:-1]:
-            for i, c in enumerate(tup[0]):
+        for result in results[:-1]:
+            for i, c in enumerate(result[0]):
                 x0[i] += c / (len(results)-1)
 
         # 3. reflect
-        worst = results[-1][0]
-        xr = [0.0] * dim
-        for i in range(dim):
-            xr[i] = x0[i] + α * (x0[i] - worst[i])
+        xr = [x0[i] + α * (x0[i] - results[-1][0][i]) for i in dimensions]
         r_score = f(xr)
         if results[0][1] <= r_score < results[-2][1]:
             nr += 1
-            del results[-1]
-            results.append([xr, r_score])
+            replace_worst(results, [xr, r_score])
             continue
 
         # 4. expand
         if r_score < results[0][1]:
-            ne += 1
-            worst = results[-1][0]
-            xe = [0.0] * dim
-            for i in range(dim):
-                xe[i] = x0[i] + γ * (x0[i] - worst[i])
+            xe = [x0[i] + γ * (x0[i] - results[-1][0][i]) for i in dimensions]
             e_score = f(xe)
-            if e_score < r_score:
-                del results[-1]
-                results.append([xe, e_score])
-                continue
-            else:
-                del results[-1]
-                results.append([xr, r_score])
-                continue
+            ne += 1
+            replace_worst(results, [xe, e_score] if e_score < r_score else [xr, r_score])
+            continue
 
         # 5. contract
-        worst = results[-1][0]
-        xc = [0.0] * dim
-        for i in range(dim):
-            xc[i] = x0[i] + ρ * (x0[i] - worst[i])
+        xc = [x0[i] + ρ * (x0[i] - results[-1][0][i]) for i in dimensions]
         c_score = f(xc)
         if c_score < results[-1][1]:
             nc += 1
-            del results[-1]
-            results.append([xc, c_score])
+            replace_worst(results, [xc, c_score])
             continue
 
         # 6. shrink
-        ns += 1
         x1 = results[0][0]
         reduced = []
-        xs = [0.0] * dim
-        for tup in results:
-            for i in range(dim):
-                xs[i] = x1[i] + σ * (tup[0][i] - x1[i])
+        for result in results:
+            xs = [x1[i] + σ * (result[0][i] - x1[i]) for i in dimensions]
             score = f(xs)
+            ns += 1
             reduced.append([xs, score])
         results = reduced
 
