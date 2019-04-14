7e3a18aef0df979c13408954689fc8d926d349a0
Ian Smith <m4r35n357@gmail.com>
2019-04-14 12:36:50 +0100

    test for reduction investigate constraints

--- Rosenbrock.py
+++ Rosenbrock.py
@@ -1,5 +1,4 @@
-
-from sys import argv
+from sys import argv, float_info
 from NelderMead import nelder_mead
 
 class Rosenbrock(object):
@@ -16,9 +15,16 @@ def himmelblau(z):
     x, y = z
     return (x**2 + y - 11)**2 + (x + y**2 - 7)**2
 
+def basic(z):
+    x, y = z
+    f = x**2 + y**2 - 4.0
+    c = 0.0 if x + y >= 2.0 else float_info.max
+    return f + c
+
 if __name__ == "__main__":
-    # Example: python Rosenbrock.py -3.0 -3.0 0.1 0.1
+    # Example: python Rosenbrock.py 3.0 3.0 0.1 0.1
     init = [float(argv[1]), float(argv[2])]
     deltas = [float(argv[3]), float(argv[4])]
     print(nelder_mead(Rosenbrock(1.0, 100.0).func, init, deltas, 1.0e-9))
     print(nelder_mead(himmelblau, init, deltas, 1.0e-9))
+    print(nelder_mead(basic, init, deltas, 1.0e-9))
